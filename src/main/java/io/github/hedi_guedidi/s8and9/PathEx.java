49e44bafbfa86aff2f859edcc802866b41fe1710
hedi-guedidi <hedi.guedidi@dauphine.eu>
2020-04-22 10:17:32 +0200

    second commit

--- src/main/java/io/github/hedi_guedidi/s8and9/PathEx.java
+++ src/main/java/io/github/hedi_guedidi/s8and9/PathEx.java
@@ -2,25 +2,45 @@ package io.github.hedi_guedidi.s8and9;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PathEx {
-	public static Path helloBye(Path path){
-		return path.resolve("Hello.txt");
-		
-	}
-	public static void main(String[] args) throws IOException {
-		final Path infile = Path.of("myfolder/");
-		final Path infile2=helloBye(infile);
-		Files.createFile(infile2);
-		try (OutputStream is = Files.newOutputStream(infile2)) {
-			try(BufferedWriter writer = Files.newBufferedWriter(infile2)){
+	public static Path helloBye(Path path) throws IOException {
+		final Path infile = path.resolve("Hello.txt");
+		final Path subfolderPath = path.resolve("subfolder/");
+		final Path byePath = subfolderPath.resolve("bye.txt");
+		Files.createFile(infile);
+		Files.createDirectory(subfolderPath);
+		try (BufferedWriter writer = Files.newBufferedWriter(infile)) {
 			writer.append("Hello,world!");
-			}
 		}
-    }
+		try (BufferedWriter writer = Files.newBufferedWriter(byePath)) {
+			writer.append("Bye bye!");
+		}
+		return infile;
+	}
 
-}
+	public static void main(String[] args) throws IOException {
+		//helloBye(Path.of("test1/"));
+		//helloBye(Path.of("test2/asubfolder/"));
+		Map<String, String> env = new HashMap<>(); 
+		Path path=Path.of("myfolder/zipfstest15948.zip");
+		
+        Files.createFile(path);
+        URI uritemp=path.toUri();
+        URI uri = URI.create("jar:"+uritemp.toString());
+        
+       try (FileSystem fs = FileSystems.newFileSystem(uri, env)) {
+    	   Path path1 =fs.getPath("myfolder/");
+    	   helloBye(path1);            
+        } 
+    }
+	}
+
